d512020e0f18d37c2bad040dac911f78e81cdb75
apatil88 <amrut.patil88@hotmail.com>
2020-04-05 16:47:12 -0400

    Configuring Apollo Client and BookList component initial commit

--- graphql-reading-list/client/src/App.js
+++ graphql-reading-list/client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+// components
+import BookList from "./components/BookList";
+
+// Apollo Client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>React App</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
